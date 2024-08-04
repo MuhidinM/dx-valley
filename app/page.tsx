bf9f7b85c557299d233394fdafb1d953d2a860d0
MuhidinM <misbahmuhidin3@gmail.com>
2024-08-04 20:09:45 +0300

    Footer done

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/footer";
 import { Hero } from "@/components/hero";
 import Navbar from "@/components/navbar";
 import Offer from "@/components/offer";
@@ -8,14 +9,15 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div className="md:mx-8">
-      <div className="mx-12 my-4">
+    <div className="lg:max-w-screen-lg xl:max-w-screen-xl mx-auto">
+      <div className="">
         <Socials />
         <Navbar />
         <Hero />
         <Section1 />
         <Section2 />
         <Offer />
+        <Footer />
       </div>
     </div>
   );
