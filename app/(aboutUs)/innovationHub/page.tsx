25c532479df18ed417714021fd2ffbb005ebbb6a
Elshaday Zewdie <elshadayzewude@gmail.com>
2024-08-15 16:23:16 +0300

    innovation page

--- app/(aboutUs)/innovationHub/page.tsx
+++ app/(aboutUs)/innovationHub/page.tsx
@@ -1,25 +1,24 @@
-import NavBar from "@/components/navbar";;
-import Footer from "@/components/footer";
-import CTA from "@/components/cta";
-import DxDescription from "@/components/dxDesc";
-import Mission from "@/components/mission";
-import { IncubationPhase } from "@/components/incubationPhase";
-import HowWeWorkSection from "@/components/howWeWork";
-const InnovationHubPage = () => {
-    return (
-        <div className="bg-slate-50">
-            <div className="bg-slate-50 dark:bg-gray-900 mb-3">
-                <NavBar />
-                <DxDescription />
-                <div className="grid grid-cols-2 gap-2 px-2 py-8">
-                    <Mission />
-                    <CTA />
-                </div>
+import DxDescription from '@/components/dxDesc'
+import FocusAreas from '@/components/focusAreas'
+import HowWeWorkSection from '@/components/howWeWork'
+import Navbar from '@/components/navbar'
+import Section1 from '@/components/section1'
+import React from 'react'
 
-                <Footer />
-            </div>
-        </div>
-    );
-};
 
-export default InnovationHubPage;
+const Page = () => {
+  return (
+    // <div>Page</div>
+<div>
+          {/* <Section1 /> */}
+          <DxDescription />
+          <FocusAreas />
+          <HowWeWorkSection /> 
+        
+
+</div>
+
+  )
+}
+
+export default Page
