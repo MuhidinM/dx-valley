2c32056bb8e3d626bd6c7fb36babd5c2fcfc1645
Elshaday Zewdie <elshadayzewude@gmail.com>
2024-08-16 19:09:02 +0300

    stat background :  bg-coopBlue

--- components/landing/stats.tsx
+++ components/landing/stats.tsx
@@ -4,16 +4,15 @@ import React from "react";
 import CountUp from "react-countup";
 const Stats: React.FC<StatsProps> = ({ items }) => {
   return (
-    <section className='dark:bg-gray-900'>
+    <section className='dark:bg-gray-900 '>
       <div className='max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6'>
-    
-        <dl className='grid gap-8 mx-auto text-gray-900 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 dark:text-white'>
+        <dl className='grid gap-8 mx-auto text-gray-900 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 dark:text-white '>
           {items.map((item, index) => (
             <div
               key={index}
-              className='flex flex-col items-center justify-center p-6 bg-white dark:bg-gray-800 rounded-xl shadow-lg hover:shadow-2xl transition-shadow duration-300 '>
+              className='flex flex-col items-center justify-center p-6 bg-coopBlue dark:bg-gray-800 rounded-xl shadow-lg hover:shadow-2xl transition-shadow duration-300 '>
               <dt
-                className='mb-3 text-4xl font-bold text-indigo-600 dark:text-indigo-400'
+                className='mb-3 text-4xl font-bold text-white dark:text-white'
                 data-to='300'
                 data-speed='1500'>
                 <CountUp
@@ -23,7 +22,7 @@ const Stats: React.FC<StatsProps> = ({ items }) => {
                   delay={0.5}
                 />
               </dt>
-              <dd className='text-lg font-medium text-gray-600 dark:text-gray-300'>
+              <dd className='text-lg font-medium text-white dark:text-gray-300'>
                 {item.label}
               </dd>
             </div>
