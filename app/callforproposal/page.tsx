46c8a60c33e0f52dcda0a4afc9ecef6ad7515971
Frehiwot-G <frehiwotgem@gmail.com>
2024-08-17 18:17:46 +0300

    some modifications on call for proposal page

--- app/callforproposal/page.tsx
+++ app/callforproposal/page.tsx
@@ -1,61 +1,9 @@
 import React from "react";
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+import Callforproposal from "@/components/events/callforproposal";
 
 const Page = () => {
-  return (
-    <Card style={{ marginTop: "40px", marginBottom: "40px" }}>
-      <CardHeader className="flex justify-between items-center">
-        <CardTitle className="text-center flex-grow">
-          Call for Proposal
-        </CardTitle>
-        <div
-          className="border p-4 rounded shadow ml-auto flex items-center relative"
-          style={{ top: "-2.5rem" }}
-        >
-          <p className="text-lg text-red-500 m-0 leading-none">Countdown:</p>
-        </div>
-      </CardHeader>
-
-      <CardContent>
-        <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, quidem
-          possimus dolorum sed est facilis eaque laborum. Vitae ea unde eaque,
-          ex molestiae labore sint aperiam reprehenderit officia voluptatum
-          esse! Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit,
-          quidem possimus dolorum sed est facilis eaque laborum. Vitae ea unde
-          eaque, ex molestiae labore sint aperiam reprehenderit officia
-          voluptatum esse! Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Fugit, quidem possimus dolorum sed est facilis eaque laborum.
-          Vitae ea unde eaque, ex molestiae labore sint aperiam reprehenderit
-          officia voluptatum esse!
-        </p>
-        <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, quidem
-          possimus dolorum sed est facilis eaque laborum. Vitae ea unde eaque,
-          ex molestiae labore sint aperiam reprehenderit officia voluptatum
-          esse! Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit,
-          quidem possimus dolorum sed est facilis eaque laborum. Vitae ea unde
-          eaque, ex molestiae labore sint aperiam reprehenderit officia
-          voluptatum esse! Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Fugit, quidem possimus dolorum sed est facilis eaque laborum.
-          Vitae ea unde eaque, ex molestiae labore sint aperiam reprehenderit
-          officia voluptatum esse!
-        </p>
-        <Button className="bg-coopBlue text-white font-bold cursor-pointer px-6 py-2 hover:bg-amber-500">
-          Apply
-        </Button>
-      </CardContent>
-    </Card>
-  );
+  return <Callforproposal />;
 };
 
 export default Page;
--- components/events/contest.tsx
+++ components/events/contest.tsx
@@ -26,11 +26,14 @@ export default function ContestsPage() {
 
     fetchEvents();
   }, []);
+  const Events = events.filter(
+    (event) => event.category !== "call for proposal"
+  );
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {events.length > 0 ? (
-        events.map((event) => <Countdown key={event.id} event={event} />)
+      {Events.length > 0 ? (
+        Events.map((event) => <Countdown key={event.id} event={event} />)
       ) : (
         <div className="col-span-full text-center py-10">
           <p className="text-lg font-semibold">No events available.</p>
