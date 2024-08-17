f83160a35c71a3e48c02faf651bf21a4d5f8f3f7
Elshaday Zewdie <elshadayzewude@gmail.com>
2024-08-17 12:41:55 +0300

    InnovationHub

--- app/(aboutUs)/innovationHub/page.tsx
+++ app/(aboutUs)/innovationHub/page.tsx
@@ -1,11 +1,12 @@
 /** @format */
 
+import { Card } from "@/components/card";
 import CTA from "@/components/cta";
 import DxDescription from "@/components/dxDesc";
 import FocusAreas from "@/components/focusAreas";
 import HowWeWorkSection from "@/components/howWeWork";
 import ContactUs from "@/components/landing/contactus";
-import Mission from "@/components/mission";
+
 import {focusArea } from "@/constants";
 import React from "react";
 
@@ -15,7 +16,7 @@ const Page = () => {
     <div className='space-y-8 mb-8 justify-center'>
       <DxDescription />
       <div className='grid grid-3 gap-6 mx-auto max-w-screen-xl xl:gap-6 md:grid md:grid-cols-2'>
-        <Mission />
+      <Card img={undefined} title={"Mission"} description={"Mission Description"} />
         <CTA title={"collabotare"} buttonText={"Apply For Call"} />
       </div>
       <HowWeWorkSection />
