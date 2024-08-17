8b6707bedfba891041a7c0c612fd072147750709
Elshaday Zewdie <elshadayzewude@gmail.com>
2024-08-17 12:43:45 +0300

    innovationHub

--- app/(aboutus)/innovationhub/page.tsx
+++ app/(aboutus)/innovationhub/page.tsx
@@ -4,10 +4,10 @@ import { Card } from "@/components/card";
 import CTA from "@/components/cta";
 import DxDescription from "@/components/dxDesc";
 import FocusAreas from "@/components/focusAreas";
+import Header from "@/components/header";
 import HowWeWorkSection from "@/components/howWeWork";
 import ContactUs from "@/components/landing/contactus";
-
-import {focusArea } from "@/constants";
+import { focusArea } from "@/constants";
 import React from "react";
 
 const Page = () => {
@@ -16,11 +16,19 @@ const Page = () => {
     <div className='space-y-8 mb-8 justify-center'>
       <DxDescription />
       <div className='grid grid-3 gap-6 mx-auto max-w-screen-xl xl:gap-6 md:grid md:grid-cols-2'>
-      <Card img={undefined} title={"Mission"} description={"Mission Description"} />
+     
         <CTA title={"collabotare"} buttonText={"Apply For Call"} />
       </div>
       <HowWeWorkSection />
       <FocusAreas items={focusArea} />
+      <Header />
+      <Card
+        img={""}
+        title={"Deboo"}
+        buttonText={"Visit Site"}
+        buttonText2={"Read More"}
+        description='This is product descriptiom '
+      />
       <ContactUs />
     </div>
   );
