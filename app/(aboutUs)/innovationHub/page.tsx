dc34cb2b29ec514dc4f6cd9d3a08becf49d2c99b
Elshaday Zewdie <elshadayzewude@gmail.com>
2024-08-17 04:42:46 +0300

    innovation

--- app/(aboutUs)/innovationHub/page.tsx
+++ app/(aboutUs)/innovationHub/page.tsx
@@ -8,17 +8,19 @@ import React from 'react'
 
 
 const Page = () => {
-    return (
-        // <div>Page</div>
-        <div className="space-y-8 mb-8 justify-center">
-           
-            <DxDescription />
-            <HowWeWorkSection />
-            <FocusAreas />
-            <ContactUs />
-        </div>
+  return (
+    // <div>Page</div>
+    <div className='space-y-8 mb-8 justify-center'>
+      <DxDescription />
+      <div className='grid grid-3 gap-6 mx-auto max-w-screen-xl xl:gap-6 md:grid md:grid-cols-2'>
+        <Mission />
+        <CTA title={"collabotare"} buttonText={"Apply For Call"} />
+      </div>
+      <HowWeWorkSection />
+      <FocusAreas items={focusArea} />
+      <ContactUs />
+    </div>
+  );
+};
 
-    )
-}
-
-export default Page
+export default Page;
