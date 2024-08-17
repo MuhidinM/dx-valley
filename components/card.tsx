dcafac9105d05812d41b0fb5393fc7a3e24bac21
Elshaday Zewdie <elshadayzewude@gmail.com>
2024-08-17 12:37:31 +0300

    products card

--- components/card.tsx
+++ components/card.tsx
@@ -4,27 +4,35 @@ import { CardProps } from "@/types/general";
 
 export const Card: React.FC<CardProps> = ({
   buttonText,
+  buttonText2,
   title,
   img,
   description,
 }) => {
   return (
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <Link href="#">{img}</Link>
-      <div className="p-5">
-        <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+    <div className='max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-4'>
+      <Link href='#'>{img}</Link>
+      <div className='p-5'>
+        <a href='#'>
+          <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
             {title}
           </h5>
         </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>
           {description}
         </p>
-        {buttonText && (
-          <Button className="bg-coopBlue hover:bg-coopBlueHover">
-            {buttonText}
-          </Button>
-        )}
+        <div className='grid grid-cols-2 sm:grid-cols-2  gap-6 items-center justify-center sm:gap-6 sm:justify-center sm:items-center'>
+          {buttonText && (
+            <Button className='bg-coopBlue hover:bg-coopBlueHover w-full sm:w-auto'>
+              {buttonText}
+            </Button>
+          )}
+          {buttonText2 && (
+            <Button className='bg-coopBlue hover:bg-coopBlueHover w-full sm:w-auto'>
+              {buttonText2}
+            </Button>
+          )}
+        </div>
       </div>
     </div>
   );
---
+++ components/header.tsx
@@ -0,0 +1,18 @@
+/** @format */
+
+const Header = () => {
+  return (
+    <div>
+      <div className='text-center mb-20'>
+        <h2 className='text-3xl b-4 shrink-0 font-bold'>
+          <span className=' text-coopBlue'> Our</span> Products
+        </h2>
+        <div className='flex justify-center mt-2  mb-12'>
+          <div className='w-20 h-1 bg-coopOrange'></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
