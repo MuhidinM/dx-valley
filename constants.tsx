f283efec8aa21f8bfbeeca11a37fbe810698cc35
Elshaday Zewdie <elshadayzewude@gmail.com>
2024-08-18 14:55:32 +0300

    constants error solved

--- constants.tsx
+++ constants.tsx
@@ -258,7 +258,7 @@ export const features = [
   {
     title: "During Incubation",
     content:
-        1"lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque euismod, nisl sit amet ultricies lacinia, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl.",
+        "lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque euismod, nisl sit amet ultricies lacinia, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl.",
     srcImage:
       "https://plus.unsplash.com/premium_photo-1661416307260-5013ab7adc3f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2dyYW1tZXIlMjBsYWJ8ZW58MHx8MHx8fDA%3D",
   },
