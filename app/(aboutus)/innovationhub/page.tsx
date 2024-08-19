5947da000ed8bb74af5c45f4bb0f268223d92967
MuhidinM <misbahmuhidin3@gmail.com>
2024-08-19 09:00:46 +0300

    innovation hub layout fix

--- app/(aboutus)/innovationhub/page.tsx
+++ app/(aboutus)/innovationhub/page.tsx
@@ -34,38 +34,12 @@ const Page = () => {
         buttonText={"hidden"}
         href={"/innovationhub"}
       />
-      <div className="grid grid-col-2 gap-6 mx-auto max-w-screen-xl xl:gap-6 md:grid md:grid-cols-2">
-        <Mission />
-        {/* <Card
-          title={"Our Mission"}
-          img={
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='100'
-              height='100'
-              viewBox='0 0 24 24'
-              fill='none'
-              stroke='currentColor'
-              strokeWidth='2'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              className='lucide lucide-scan-eye'>
-              <path d='M3 7V5a2 2 0 0 1 2-2h2' />
-              <path d='M17 3h2a2 2 0 0 1 2 2v2' />
-              <path d='M21 17v2a2 2 0 0 1-2 2h-2' />
-              <path d='M7 21H5a2 2 0 0 1-2-2v-2' />
-              <circle cx='12' cy='12' r='1' />
-              <path d='M18.944 12.33a1 1 0 0 0 0-.66 7.5 7.5 0 0 0-13.888 0 1 1 0 0 0 0 .66 7.5 7.5 0 0 0 13.888 0' />
-            </svg>
-          }
-          description='Missio of ours '
-        /> */}
-        <CTA
-          title={"collabotare"}
-          buttonText={"Apply For Call"}
-          href={"#collab-form"}
-        />
-      </div>
+      <Mission />
+      <CTA
+        title={"collabotare"}
+        buttonText={"Apply For Call"}
+        href={"#collab-form"}
+      />
       <HowWeWorkSection />
       {/* <FocusAreas items={focusArea} /> */}
       <Header />
