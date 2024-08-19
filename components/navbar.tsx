ead62635d3bb9a37511201abd8af3b2caea10a05
MuhidinM <misbahmuhidin3@gmail.com>
2024-08-19 12:31:21 +0300

    check

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = () => {
             <Button
               className="bg-coopBlue hover:bg-coopBlueHover"
               onClick={() => {
-                router.push("/callforproposal"); // Updated navigation
+                router.push("/callforproposal");
               }}
             >
               Call for Proposal
