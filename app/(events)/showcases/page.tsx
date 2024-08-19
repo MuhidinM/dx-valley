71a560505e14e7b17a0b49751d9e483eb4bbe0af
MuhidinM <misbahmuhidin3@gmail.com>
2024-08-19 10:30:55 +0300

    showcase is done

--- app/(events)/showcases/page.tsx
+++ app/(events)/showcases/page.tsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import incubationPhoto from "@/public/image/incubation-center.png";
 
 const Page = () => {
   return (
-    <div>Page</div>
-  )
-}
+    <div className="flex items-center justify-center">
+      <div className="text-center my-8">
+        <div className="">
+          <h2 className="text-4xl font-bold">
+            <span className="text-coopBlue">Incubated</span> Products
+          </h2>
+          <div className="flex justify-center mt-2  mb-12">
+            <div className="w-20 h-1 bg-coopOrange"></div>
+          </div>
+        </div>
 
-export default Page
+        <div className="grid grid-cols-3 gap-4">
+          <Card className="w-full">
+            <CardHeader>
+              <CardTitle>Relevance AI</CardTitle>
+              <CardDescription>
+                Relevance AI is a machine learning startup on mission to help
+                companies build an AI workforce that automates workflows with no
+                code.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Image src={incubationPhoto} alt="incubation" />
+              <div className="flex justify-between my-2">
+                <div className="">
+                  <h3 className="font-bold">Founders</h3>
+                  <ul className="text-gray-500">
+                    <li>Name</li>
+                    <li>Name</li>
+                    <li>Name</li>
+                  </ul>
+                </div>
+                <div className="">
+                  <h3 className="font-bold">Co-Investors</h3>
+                  <ul className="text-gray-500">
+                    <li>Name</li>
+                    <li>Name</li>
+                    <li>Name</li>
+                  </ul>
+                </div>
+              </div>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <div className=""></div>
+              <Button className="bg-coopBlue hover:bg-coopBlueHover">
+                Read More
+              </Button>
+            </CardFooter>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
