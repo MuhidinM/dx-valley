596cf5e8efd359b266ed78965a1b27fbbc3a3131
MuhidinM <misbahmuhidin3@gmail.com>
2024-08-19 09:10:46 +0300

    innovationhub fix

--- components/mission.tsx
+++ components/mission.tsx
@@ -6,34 +6,17 @@ import {
   CardDescription,
   CardContent,
 } from "@/components/ui/card";
+import { Target, View } from "lucide-react";
 
 const Mission = () => {
   return (
     <div className="md:grid grid-cols-2 gap-4">
       <Card>
         <CardTitle className="items-center justify-center p-6">
-          <span className="text-4xl font-semibold">Mission</span>
+          <span className="text-4xl font-bold text-coopOrange">Mission</span>
         </CardTitle>
         <CardContent className="flex items-center h-fit justify-center p-6">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="100"
-            height="100"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className=""
-          >
-            <path d="M3 7V5a2 2 0 0 1 2-2h2" />
-            <path d="M17 3h2a2 2 0 0 1 2 2v2" />
-            <path d="M21 17v2a2 2 0 0 1-2 2h-2" />
-            <path d="M7 21H5a2 2 0 0 1-2-2v-2" />
-            <circle cx="12" cy="12" r="1" />
-            <path d="M18.944 12.33a1 1 0 0 0 0-.66 7.5 7.5 0 0 0-13.888 0 1 1 0 0 0 0 .66 7.5 7.5 0 0 0 13.888 0" />
-          </svg>
+          <Target size={96} className="text-coopBlue" strokeWidth={1.5} />
         </CardContent>
         <CardFooter>
           <p>
@@ -51,29 +34,11 @@ const Mission = () => {
       <Card>
         <CardHeader>
           <CardTitle>
-            <span className="text-4xl font-semibold">Vision </span>
+            <span className="text-4xl font-bold text-coopOrange">Vision </span>
           </CardTitle>
         </CardHeader>
         <CardContent className="grid items-center h-fit justify-center p-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="100"
-            height="100"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="lucide lucide-scan-eye"
-          >
-            <path d="M3 7V5a2 2 0 0 1 2-2h2" />
-            <path d="M17 3h2a2 2 0 0 1 2 2v2" />
-            <path d="M21 17v2a2 2 0 0 1-2 2h-2" />
-            <path d="M7 21H5a2 2 0 0 1-2-2v-2" />
-            <circle cx="12" cy="12" r="1" />
-            <path d="M18.944 12.33a1 1 0 0 0 0-.66 7.5 7.5 0 0 0-13.888 0 1 1 0 0 0 0 .66 7.5 7.5 0 0 0 13.888 0" />
-          </svg>
+          <View size={96} className="text-coopBlue" strokeWidth={1.5} />
         </CardContent>
         <CardFooter>
           <p>
