374ab02c579920059a908d5457b243b4869a43d6
MuhidinM <misbahmuhidin3@gmail.com>
2024-08-19 14:56:55 +0300

    fix

--- app/api/subscriber/route.ts
+++ app/api/subscriber/route.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from '../../../node_modules/.prisma/client';
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 export async function POST(req: Request): Promise<NextResponse> {
   try {
     const { email } = await req.json();
     const newSubscriber = await prisma.subscriber.create({
       data: {
-        email
+        email,
       },
     });
     return NextResponse.json(
