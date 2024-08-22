14f769a7e6e6df0fea6718c1e2e5973dfa08c4d0
Esubalew Gosaye <mesaye2010@gmail.com>
2024-08-22 14:01:46 +0300

    strapi done for menu items

--- constants.tsx
+++ constants.tsx
@@ -21,6 +21,56 @@ import HeroImage3 from "@/public/image/hero3.jpg";
 import HeroImage4 from "@/public/image/hero4.jpg";
 
 
+
+// const menuItemFetch = async ()=>{
+//   const res = await axios.get<ApiResponse>('http://10.1.151.64:1337/api/menu-items?populate[0]=highlight.img&populate[1]=highlight.links');
+//   const data = res.data;
+//   console.log(data)
+// }
+// useEffect(()=>{menuItemFetch()},[])
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const res = await axios.get<ApiResponse>('http://10.1.151.64:1337/api/menu-items?populate[0]=highlight.img&populate[1]=highlight.links');
+//   const data = res.data;
+
+  // const menuItems = data.data.map(item => ({
+  //   trigger: item.attributes.trigger,
+  //   highlight: {
+  //     href: item.attributes.highlight.href,
+  //     title: item.attributes.highlight.title,
+  //     description: item.attributes.highlight.description,
+  //     img: item.attributes.highlight.img.data?.attributes.formats.medium.url || '',  // Fallback to an empty string if no image
+  //     links: item.attributes.highlight.links.map(link => ({
+  //       href: link.href,
+  //       title: link.title,
+  //       description: link.description,
+  //     })),
+  //   }
+  // }));
+//   console.log(data)
+//   return {
+//     props: {
+//       menuItems,
+//     },
+//   };
+// };
+
+interface MenuComponentProps {
+  menuItems: {
+    trigger: string;
+    highlight: {
+      href: string;
+      title: string;
+      description: string;
+      img: string;
+      links: {
+        href: string;
+        title: string;
+        description: string;
+      }[];
+    };
+  }[];
+}
+
 export const menuItems = [
   {
     trigger: "About Us",
