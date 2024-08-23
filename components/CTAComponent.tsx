636c69dadf836d5639b5925a514cb7444d99f52e
MuhidinM <misbahmuhidin3@gmail.com>
2024-08-23 14:47:29 +0300

    small fix

--- components/CTAComponent.tsx
+++ components/CTAComponent.tsx
@@ -1,15 +1,15 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 export default function CTAComponent() {
   return (
-    <section className='w-full px-12 py-12 md:py-24 lg:py-24 dark:bg-gray-400'>
-      <div className='container mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
-        <div className='grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16 items-center'>
-          <div className='space-y-6'>
+    <section className="w-full px-12 py-12 md:py-24 lg:py-24">
+      <div className="container mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16 items-center">
+          <div className="space-y-6">
             <h2 className="text-4xl sm:text-4xl lg:text-5xl font-bold tracking-tight text-coopOrange font-['Arial']">
               Join Us On Our Digital Empowerment Journey
             </h2>
-            <p className='text-xl sm:text-lg lg:text-xl text-coopBlue '>
+            <p className="text-xl sm:text-lg lg:text-xl text-coopBlue ">
               <em>
                 We are where digital solutions lead the way in empowering
                 communities and enhancing lives.
@@ -19,18 +19,18 @@ export default function CTAComponent() {
               Walk the empowering digital path with us, One Step at a Time.
             </p>
           </div>
-          <div className='grid grid-cols-2 gap-4'>
+          <div className="grid grid-cols-2 gap-4">
             <Button className="w-full bg-gray-800 hover:bg-gray-700 text-white font-['Arial'] text-lg py-0 transition-colors duration-300">
-              I'm a Trainer
+              I&apos;m a Trainer
             </Button>
             <Button className="w-full bg-gray-800 hover:bg-gray-700 text-white font-['Arial'] text-lg py-0 transition-colors duration-300">
-              I'm an Organization
+              I&apos;m an Organization
             </Button>
             <Button className="w-full bg-gray-800 hover:bg-gray-700 text-white font-['Arial'] text-lg py-0 transition-colors duration-300">
-              I'm Media
+              I&apos;m Media
             </Button>
             <Button className="w-full bg-gray-800 hover:bg-gray-700 text-white font-['Arial'] text-lg py-0 transition-colors duration-300">
-              I'm a Stakeholder
+              I&apos;m a Stakeholder
             </Button>
           </div>
         </div>
