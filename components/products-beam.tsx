18c6cc21c790ff42fccc97d637f5356f8950c3bb
MuhidinM <misbahmuhidin3@gmail.com>
2024-08-27 17:38:37 +0300

    beam done

--- components/products-beam.tsx
+++ components/products-beam.tsx
@@ -1,15 +1,19 @@
 "use client";
 
 import React, { forwardRef, useRef, useState } from "react";
-
 import { cn } from "@/lib/utils";
 import { AnimatedBeam } from "./ui/animated-beam";
 import Image from "next/image";
 
 const Circle = forwardRef<
   HTMLDivElement,
-  { className?: string; children?: React.ReactNode }
->(({ className, children }, ref) => {
+  {
+    className?: string;
+    children?: React.ReactNode;
+    onHover: () => void;
+    onLeave: () => void;
+  }
+>(({ className, children, onHover, onLeave }, ref) => {
   return (
     <div
       ref={ref}
@@ -17,6 +21,8 @@ const Circle = forwardRef<
         "z-10 flex size-12 items-center justify-center rounded-full border-2 bg-white p-3 shadow-[0_0_20px_-12px_rgba(0,0,0,0.8)]",
         className
       )}
+      onMouseEnter={onHover}
+      onMouseLeave={onLeave}
     >
       {children}
     </div>
@@ -29,14 +35,13 @@ export function ProductsBeam() {
   const [activeTag, setActiveTag] = useState<string | null>(null);
 
   const descriptions = {
-    div1: "We'd love to hear from you! Have a question about our products or services? Need help with an order? Our friendly customer support team is here to assist you.",
-    div2: "Deboo is a crowdfunding platform that provides a secure environment for individuals and organizations to receive donations and for contributors to support trustworthy causes. It serves as a bridge between potential donors and equity contributors, facilitating the connection between those seeking financial support and those willing to provide it.",
+    div1: "Description for Equb",
+    div2: "Deboo is a crowdfunding platform that provides a secure environment for individuals and organizations to receive donations and for contributors to support trustworthy causes.",
     div3: "Description for Coop Stream",
     div4: "Description for Dx-valley",
     div5: "Description for Diaspora Banking",
     div6: "Description for VSLA",
     div7: "Description for Souqpass",
-    div8: "Description for Equb",
   };
 
   const containerRef = useRef<HTMLDivElement>(null);
@@ -50,48 +55,32 @@ export function ProductsBeam() {
 
   return (
     <div
-      className="relative h-[500px] rounded-lg border bg-background p-10 md:shadow-xl grid grid-cols-2 gap-4"
+      className="relative h-[500px] rounded-lg border bg-background p-10 grid grid-cols-2 gap-4"
       ref={containerRef}
     >
       <div className="flex size-full flex-col max-w-lg items-stretch justify-between ">
         <div className="flex flex-row items-center justify-between">
-          <Circle ref={div1Ref} className="w-24 h-24">
+          <Circle
+            ref={div1Ref}
+            className="w-24 h-24"
+            onHover={() => setActiveTag("div1")}
+            onLeave={() => setActiveTag(null)}
+          >
             <Image
-              src={"/image/companies/coop-stream.png"}
+              src={"/image/companies/equb.png"}
               alt=""
               width={100}
               height={100}
             />
           </Circle>
-          <Circle ref={div5Ref} className="w-24 h-24">
+          <Circle
+            ref={div5Ref}
+            className="w-24 h-24"
+            onHover={() => setActiveTag("div5")}
+            onLeave={() => setActiveTag(null)}
+          >
             <Image
-              src={"/image/companies/coop-stream.png"}
-              alt=""
-              width={100}
-              height={100}
-            />
-          </Circle>
-        </div>
-        <div className="flex flex-row items-center justify-between">
-          <Circle ref={div2Ref} className="w-24 h-24">
-            <Image
-              src={"/image/companies/coop-stream.png"}
-              alt=""
-              width={100}
-              height={100}
-            />
-          </Circle>
-          <Circle ref={div4Ref} className="w-36 h-36">
-            <Image
-              src={"/image/companies/coop-stream.png"}
-              alt=""
-              width={100}
-              height={100}
-            />
-          </Circle>
-          <Circle ref={div6Ref} className="w-24 h-24">
-            <Image
-              src={"/image/companies/coop-stream.png"}
+              src={"/image/companies/diaspora.png"}
               alt=""
               width={100}
               height={100}
@@ -99,7 +88,53 @@ export function ProductsBeam() {
           </Circle>
         </div>
         <div className="flex flex-row items-center justify-between">
-          <Circle ref={div3Ref} className="w-24 h-24">
+          <Circle
+            ref={div2Ref}
+            className="w-24 h-24"
+            onHover={() => setActiveTag("div2")}
+            onLeave={() => setActiveTag(null)}
+          >
+            <Image
+              src={"/image/companies/debbo.png"}
+              alt=""
+              width={100}
+              height={100}
+            />
+          </Circle>
+          <Circle
+            ref={div4Ref}
+            className="w-36 h-36"
+            onHover={() => setActiveTag("div4")}
+            onLeave={() => setActiveTag(null)}
+          >
+            <Image
+              src={"/image/companies/DX.jpg"}
+              alt=""
+              width={100}
+              height={100}
+            />
+          </Circle>
+          <Circle
+            ref={div6Ref}
+            className="w-24 h-24"
+            onHover={() => setActiveTag("div6")}
+            onLeave={() => setActiveTag(null)}
+          >
+            <Image
+              src={"/image/companies/VSLA-image.png"}
+              alt=""
+              width={100}
+              height={100}
+            />
+          </Circle>
+        </div>
+        <div className="flex flex-row items-center justify-between">
+          <Circle
+            ref={div3Ref}
+            className="w-24 h-24"
+            onHover={() => setActiveTag("div3")}
+            onLeave={() => setActiveTag(null)}
+          >
             <Image
               src={"/image/companies/coop-stream.png"}
               alt=""
@@ -107,9 +142,14 @@ export function ProductsBeam() {
               height={100}
             />
           </Circle>
-          <Circle ref={div7Ref} className="w-24 h-24">
+          <Circle
+            ref={div7Ref}
+            className="w-24 h-24"
+            onHover={() => setActiveTag("div7")}
+            onLeave={() => setActiveTag(null)}
+          >
             <Image
-              src={"/image/companies/coop-stream.png"}
+              src={"/image/companies/souqpass.png"}
               alt=""
               width={100}
               height={100}
@@ -118,7 +158,11 @@ export function ProductsBeam() {
         </div>
       </div>
 
-      <div className="border-l-2 px-10">test</div>
+      <div className="border-l-2 px-10">
+        {activeTag
+          ? descriptions[activeTag]
+          : "Hover over a circle to see the description"}
+      </div>
 
       <AnimatedBeam
         containerRef={containerRef}
