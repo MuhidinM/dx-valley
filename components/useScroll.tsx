4c7c501e8820427687bc80e569cbc01c51a1c052
NuredinAbdurehaman <nuredinabdurehaman@gmail.com>
2024-09-02 16:16:32 +0300

    tiny

--- components/useScroll.tsx
+++ components/useScroll.tsx
@@ -4,7 +4,7 @@ const useScroll = () => {
   const [scroll, setScroll] = useState(false);
 
   const handleScroll = () => {
-    if (window.scrollY > 50) {
+    if (window.scrollY > 50z) {
       setScroll(true);
     } else {
       setScroll(false);
