f62bec9e2a105d0f80a0505af00097533926c616
MuhidinM <misbahmuhidin3@gmail.com>
2024-09-19 17:16:57 +0300

    section and projects

--- components/section.tsx
+++ components/section.tsx
@@ -15,21 +15,21 @@ export const SectionRight: React.FC<SectionProps> = ({
   href,
 }) => {
   return (
-    <section className='dark:bg-gray-900'>
-      <div className='gap-8 items-center py-4 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-10 lg:px-6'>
+    <section className="dark:bg-gray-900">
+      <div className="gap-8 items-center py-4 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-10 lg:px-6">
         {svg}
-        <div className='mt-4 md:mt-0'>
-          <h2 className='mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white'>
+        <div className="mt-4 md:mt-0">
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
             {title}
           </h2>
-          <div className='prose mb-6 font-light text-gray-900 md:text-lg dark:text-gray-400'>
+          <div className="prose mb-6 font-light text-gray-900 md:text-lg dark:text-gray-400">
             <ReactMarkdown remarkPlugins={[remarkGfm]}>
               {description?.toString()}
             </ReactMarkdown>
           </div>
           {buttonText !== "hidden" && (
             <Link href={href}>
-              <Button className='bg-coopBlue hover:bg-coopBlueHover '>
+              <Button className="bg-coopBlue hover:bg-coopBlueHover ">
                 {buttonText}
               </Button>
             </Link>
@@ -48,13 +48,13 @@ export const SectionLeft: React.FC<SectionProps> = ({
   href,
 }) => {
   return (
-    <section className='dark:bg-gray-900'>
-      <div className='gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6'>
-        <div className='mt-4 md:mt-0'>
-          <h2 className='mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white'>
+    <section className="dark:bg-gray-900">
+      <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+        <div className="mt-4 md:mt-0">
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
             {title}
           </h2>
-          <div className='prose mb-6 font-light text-gray-900 md:text-lg dark:text-gray-400'>
+          <div className="prose mb-6 font-light text-gray-900 md:text-lg dark:text-gray-400">
             <ReactMarkdown remarkPlugins={[remarkGfm]}>
               {description?.toString()}
             </ReactMarkdown>
@@ -62,7 +62,7 @@ export const SectionLeft: React.FC<SectionProps> = ({
 
           {buttonText !== "hidden" && (
             <Link href={href}>
-              <Button className='bg-coopBlue hover:bg-coopBlueHover '>
+              <Button className="bg-coopBlue hover:bg-coopBlueHover ">
                 {buttonText}
               </Button>
             </Link>
@@ -82,23 +82,23 @@ export const CardComponent: React.FC<SectionProps> = ({
   href,
 }) => {
   return (
-    <div className='bg-white dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden flex flex-col h-full'>
-      {svg && <div className='p-6'>{svg}</div>}
+    <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden flex flex-col h-full">
+      {svg && <div className="p-6 flex items-center justify-center">{svg}</div>}
 
-      <div className='flex flex-col flex-1 p-6'>
-        <h2 className='mb-4 text-2xl tracking-tight font-extrabold text-gray-900 dark:text-white'>
+      <div className="flex flex-col flex-1 p-6">
+        <h2 className="mb-4 text-2xl tracking-tight font-extrabold text-gray-900 dark:text-white">
           {title}
         </h2>
-        <div className='prose mb-6 font-light text-gray-900 md:text-lg dark:text-gray-400'>
+        <div className="prose mb-6 font-light text-gray-900 md:text-lg dark:text-gray-400">
           <ReactMarkdown remarkPlugins={[remarkGfm]}>
             {description?.toString()}
             {/* {description} */}
           </ReactMarkdown>
         </div>
-        <div className='mt-auto'>
+        <div className="mt-auto">
           {buttonText && buttonText !== "hidden" && (
             <Link href={href}>
-              <Button className='bg-[#0f172a] w-full hover:bg-gray-700'>
+              <Button className="bg-[#0f172a] w-full hover:bg-gray-700">
                 {buttonText}
               </Button>
             </Link>
