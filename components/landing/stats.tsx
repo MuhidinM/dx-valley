0da5fe3232226849af79728886c6e7d3236f47cb
MuhidinM <misbahmuhidin3@gmail.com>
2024-09-19 17:05:36 +0300

    stat

--- components/landing/stats.tsx
+++ components/landing/stats.tsx
@@ -34,25 +34,27 @@ export default function Stats({ items }: { items: CardNoLinkData[] }) {
 
   return (
     <section
-      className='dark:bg-gray-900 max-w-screen-xl md:w-full '
-      ref={statsRef}>
-      <div className='max-w-screen-xl md:w-full px-4 py-8 mx-auto text-center lg:py-16 lg:px-6'>
-        <dl className='grid gap-8 mx-auto text-gray-900 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 dark:text-white'>
+      className="dark:bg-gray-900 max-w-screen-xl md:w-full "
+      ref={statsRef}
+    >
+      <div className="max-w-screen-xl md:w-full px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
+        <dl className="grid gap-8 mx-auto text-gray-900 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 dark:text-white">
           {items.map((item, index) => (
             <div
               key={index}
-              className='flex flex-col items-center justify-center p-6 bg-gray-200 dark:bg-gray-800 rounded-xl shadow-lg hover:shadow-2xl transition-shadow duration-300'>
+              className="flex flex-col items-center justify-center p-6 bg-slate-200 dark:bg-gray-800 rounded-xl shadow-lg"
+            >
               <div>
                 <Image
-                  alt=''
+                  alt=""
                   width={60}
                   height={0}
                   src={`${process.env.NEXT_PUBLIC_STRAPI_IP_DEV}${item.img}`}
-                  className='pb-6'
+                  className="pb-6"
                 />
                 {/* <div className="w-60 h-60">{item.img}</div> */}
               </div>
-              <dt className='mb-3 text-4xl font-bold text-gray-900 dark:text-white'>
+              <dt className="mb-3 text-4xl font-bold text-gray-900 dark:text-white">
                 {inView ? (
                   <CountUp
                     start={0}
@@ -64,7 +66,7 @@ export default function Stats({ items }: { items: CardNoLinkData[] }) {
                   0
                 )}
               </dt>
-              <dd className='text-2xl font-extrabold text-gray-700 dark:text-gray-300 '>
+              <dd className="text-2xl font-extrabold text-gray-700 dark:text-gray-300 ">
                 {item.title}
               </dd>
             </div>
