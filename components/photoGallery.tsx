eb17bcab611b7a2d26dcdfb95bfa9502124e9931
Elshaday Zewdie <elshadayzewude@gmail.com>
2024-09-20 20:38:26 +0300

    comment changes

--- components/photoGallery.tsx
+++ components/photoGallery.tsx
@@ -75,49 +75,47 @@ export default function PhotoGallery() {
   });
 
   return (
-    <div className="container mx-auto py-10">
-      <div className="mb-8">
-        <Select onValueChange={setFilter} defaultValue="all">
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="Select filter" />
+    <div className='container mx-auto py-10'>
+      <div className='mb-8'>
+        <Select onValueChange={setFilter} defaultValue='all'>
+          <SelectTrigger className='w-[180px]'>
+            <SelectValue placeholder='Select filter' />
           </SelectTrigger>
           <SelectContent>
-            <SelectItem value="all">All</SelectItem>
+            <SelectItem value='all'>All</SelectItem>
             {/* {filteredItems.map((item, index) => (
               <div key={index}>
                 <SelectItem value={item.type}>{item.type}</SelectItem>
                
               </div>
             ))} */}
-            <SelectItem value="innovation">Innovation Hub</SelectItem>
-            <SelectItem value="incubation">Incubation Lab</SelectItem>
+            <SelectItem value='innovation'>Innovation Hub</SelectItem>
+            <SelectItem value='incubation'>Incubation Lab</SelectItem>
           </SelectContent>
         </Select>
       </div>
       <GalleryGrid items={filteredItems} onImageClick={openModal} />
       {selectedImage !== null && (
         <div
-          className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
-          onClick={closeModal}
-        >
-          <div className="relative max-w-5xl w-full h-full" {...handlers}>
+          className='fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50'
+          onClick={closeModal}>
+          <div className='relative max-w-5xl w-full h-full' {...handlers}>
             <Button
-              className="absolute top-4 right-4 z-10"
-              size="icon"
+              className='absolute top-4 right-4 z-10'
+              size='icon'
               onClick={(e) => {
                 closeModal();
-              }}
-            >
-              <X className="h-4 w-4" />
+              }}>
+              <X className='h-4 w-4' />
             </Button>
             <img
               src={`${process.env.NEXT_PUBLIC_STRAPI_IP_DEV}${filteredItems[selectedImage].img}`}
               alt={filteredItems[selectedImage].title}
-              className="object-contain mx-auto w-[1000px]"
+              className=' min-w-full'
               onClick={(e) => e.stopPropagation()}
             />
-            <div className="absolute bottom-4 left-4 right-4 bg-white bg-opacity-75 p-4 text-black">
-              <h3 className="text-lg font-bold mb-2">
+            <div className='absolute bottom-4 left-4 right-4 bg-white bg-opacity-75 p-4 text-black'>
+              <h3 className='text-lg font-bold mb-2'>
                 {filteredItems[selectedImage].title}
               </h3>
               <p>{filteredItems[selectedImage].description}</p>
