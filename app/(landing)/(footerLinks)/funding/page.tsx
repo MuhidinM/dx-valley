a5ceee48470aa8061c914c85b354222c72a9b7a8
Elshaday Zewdie <elshadayzewude@gmail.com>
2024-09-21 09:38:14 +0300

    funding page added

---
+++ app/(landing)/(footerLinks)/funding/page.tsx
@@ -0,0 +1,18 @@
+/** @format */
+
+import React from "react";
+const Page: React.FC = () => {
+  return (
+    <div className='flex items-center justify-center min-h-screen bg-cyan-700'>
+      <div className='text-center'>
+        <h1 className='text-5xl font-bold text-amber-400 mb-4'>
+          Something Awesome is Coming Soon!
+        </h1>
+        <p className='text-2xl text-white'>
+          Stay tuned for the big reveal. We can't wait to show you what's next!
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Page;
