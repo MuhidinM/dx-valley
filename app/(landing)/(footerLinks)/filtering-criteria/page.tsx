096a99269d34a059ea250d1bb4ebb6d2d2412848
Elshaday Zewdie <elshadayzewude@gmail.com>
2024-09-21 11:52:39 +0300

    filtering criteria

---
+++ app/(landing)/(footerLinks)/filtering-criteria/page.tsx
@@ -0,0 +1,12 @@
+/** @format */
+
+import ComingSoonComp from "@/components/comingSoonComp";
+import React from "react";
+const Page: React.FC = () => {
+  return (
+    <div className='flex items-center justify-center min-h-72'>
+      <ComingSoonComp />
+    </div>
+  );
+};
+export default Page;
