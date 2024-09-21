b1e0cf5c5cb2ac124a2ab7153b56954bce604ae7
Elshaday Zewdie <elshadayzewude@gmail.com>
2024-09-21 09:21:04 +0300

    exposed IP removed

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    //domains: ['10.1.151.64'],  // added to allow image fetch from strapi
+   
     remotePatterns: [
       {
         protocol: "https",
