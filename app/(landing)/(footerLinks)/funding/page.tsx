92803b4b762bb0deec1acbf07f6038e05f9bd89f
Elshaday Zewdie <elshadayzewude@gmail.com>
2024-09-21 11:50:01 +0300

    fundinng page comming soon

--- app/(landing)/(footerLinks)/funding/page.tsx
+++ app/(landing)/(footerLinks)/funding/page.tsx
@@ -6,9 +6,6 @@ const Page: React.FC = () => {
     <div className='flex items-center justify-center min-h-72'>
       <div className='text-center'>
         <h1 className='text-5xl font-bold text-[#E4933C] mb-4'>Coming Soon!</h1>
-        <p className='text-2xl text-white'>
-          Stay tuned for the big reveal. We can't wait to show you what's next!
-        </p>
       </div>
     </div>
   );
