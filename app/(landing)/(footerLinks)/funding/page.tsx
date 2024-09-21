954d0becc0f0a7a77c671c85586605299f5fe10b
Elshaday Zewdie <elshadayzewude@gmail.com>
2024-09-21 11:51:37 +0300

    coming soon made an independent component

--- app/(landing)/(footerLinks)/funding/page.tsx
+++ app/(landing)/(footerLinks)/funding/page.tsx
@@ -1,12 +1,11 @@
 /** @format */
 
+import ComingSoonComp from "@/components/comingSoonComp";
 import React from "react";
 const Page: React.FC = () => {
   return (
     <div className='flex items-center justify-center min-h-72'>
-      <div className='text-center'>
-        <h1 className='text-5xl font-bold text-[#E4933C] mb-4'>Coming Soon!</h1>
-      </div>
+      <ComingSoonComp />
     </div>
   );
 };
---
+++ components/comingSoonComp.tsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from "react";
+const ComingSoonComp: React.FC = () => {
+  return (
+    <div className='flex items-center justify-center min-h-72'>
+      <div className='text-center'>
+        <h1 className='text-5xl font-bold text-[#E4933C] mb-4'>Coming Soon!</h1>
+      </div>
+    </div>
+  );
+};
+export default ComingSoonComp;
