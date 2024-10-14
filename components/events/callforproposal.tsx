69bc5defc2ada154e6513dab02ef9957f0bacd66
MuhidinM <misbahmuhidin3@gmail.com>
2024-10-14 10:30:03 +0300

    call markdown

--- components/events/callforproposal.tsx
+++ components/events/callforproposal.tsx
@@ -8,6 +8,9 @@ import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import CountDownCallForProposal from "./countDownCallForProposal";
 
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
 export default function Callforproposal() {
   const [events, setEvents] = useState<Event[]>([]);
   const [timeLeft, setTimeLeft] = useState<{ [key: string]: string }>({});
@@ -52,7 +55,7 @@ export default function Callforproposal() {
   );
 
   return (
-    <div className='lg:container w-full lg:my-16 my-8 px-4'>
+    <div className="lg:container w-full lg:my-16 my-8 px-4">
       {callForProposalEvents.length > 0 ? (
         callForProposalEvents.map((event) => {
           const timeLeftForEvent = timeLeft[event.id]; // Get time left for this event
@@ -60,7 +63,7 @@ export default function Callforproposal() {
           console.log("Time left for event", event.id, ":", timeLeftForEvent);
 
           // If the timeLeft is undefined or "00h 00m 00s", skip rendering the event
-       
+
           if (timeLeftForEvent === "00h 00m 00s") {
             return null;
           }
@@ -78,29 +81,32 @@ export default function Callforproposal() {
 
               {/* Event Card */}
               <Card>
-                <CardHeader className='flex flex-row justify-between p-4 gap-8 rounded-lg m-4 sm:m-4'>
-                  <CardTitle className='text-lg lg:text-3xl sm:text-lg'>
+                <CardHeader className="flex flex-row justify-between p-4 gap-8 rounded-lg m-4 sm:m-4">
+                  <CardTitle className="text-lg lg:text-3xl sm:text-lg">
                     Call for Start Up Proposal
                   </CardTitle>
-                  <div className='h-full align-middle'>
-                    <p className='text-lg lg:text-2xl flex text-left text-red-500 font-mono leading-none'>
+                  <div className="h-full align-middle">
+                    <p className="text-lg lg:text-2xl flex text-left text-red-500 font-mono leading-none">
                       {/* Show time left or "Calculating..." if it's still loading */}
                       {timeLeftForEvent || "Calculating..."}
                     </p>
                   </div>
                 </CardHeader>
 
-                <CardContent className='align-center justify-around items-center'>
-                  <p className='mb-6 font-light text-gray-500 md:text-lg h-4/5 justify-around items-center dark:text-gray-400'>
-                    {event.description}
+                <CardContent className="align-center justify-around items-center">
+                  <p className="prose mb-6 text-gray-500 md:text-lg h-4/5 dark:text-gray-400">
+                    <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                      {event.description}
+                    </ReactMarkdown>
                   </p>
 
                   <Button
-                    className='bg-coopBlue text-white font-bold cursor-pointer px-6 py-2 hover:bg-coopBlueHover'
+                    className="bg-coopBlue text-white font-bold cursor-pointer px-6 py-2 hover:bg-coopBlueHover"
                     onClick={() => {
                       const formRoute = "/incubationform";
                       router.push(`${formRoute}?eventId=${event.id}`);
-                    }}>
+                    }}
+                  >
                     Apply
                   </Button>
                 </CardContent>
@@ -109,8 +115,8 @@ export default function Callforproposal() {
           );
         })
       ) : (
-        <div className='col-span-full text-center py-10 lg:min-h-80 md:min-h-72'>
-          <p className='text-lg font-semibold h-fit'>No Call Available.</p>
+        <div className="col-span-full text-center py-10 lg:min-h-80 md:min-h-72">
+          <p className="text-lg font-semibold h-fit">No Call Available.</p>
         </div>
       )}
     </div>
