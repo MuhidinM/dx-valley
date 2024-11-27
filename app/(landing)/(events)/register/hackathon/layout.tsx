6cd4ad79bd8b2165ad345e5f622a1cf8c5119a2f
Elshaday Zewdie <elshadayzewude@gmail.com>
2024-11-27 18:48:29 +0300

    metadata and titles for all the pages

---
+++ app/(landing)/(events)/register/hackathon/layout.tsx
@@ -0,0 +1,20 @@
+/** @format */
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: " Hackathone ",
+  description: "Register to be part of hackathone",
+};
+
+export default function IncubationCenterLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className='min-h-screen bg-gray-50 text-black dark:bg-gray-900 dark:text-white'>
+      <main className='container mx-auto px-4 py-8'>{children}</main>
+    </div>
+  );
+}
