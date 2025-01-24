675e6a51462c9d7b1c5baf92f1297431eedb2d27
Elshaday Zewdie <elshadayzewude@gmail.com>
2025-01-24 10:17:29 +0300

    clean code

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -82,7 +82,7 @@ const Page = () => {
               description={homepageItems?.proposal.description || " "}
             />
           </div>
-          hello
+          
           <div className='lg:col-span-1  md:col-span-2'>
             <EventsSider />
           </div>
